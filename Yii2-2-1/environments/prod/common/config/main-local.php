892d4600a076ef11df117cd1656da3b00d0751f0
mpasokolov <mpasokolov@gmail.com>
2019-02-02 21:09:13 +0300

    homework1 added

---
+++ Yii2-2-1/environments/prod/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2-project',
+            'username' => 'yii2-project',
+            'password' => 'yii2-project',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
