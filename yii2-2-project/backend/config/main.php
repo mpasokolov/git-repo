ee20c70ae727a3b359c6e6d1c599924e66b981d5
mpasokolov <mpasokolov@gmail.com>
2019-02-02 22:29:42 +0300

    pretty url and admin module with general auth

--- yii2-2-project/backend/config/main.php
+++ yii2-2-project/backend/config/main.php
@@ -14,16 +14,17 @@ return [
     'modules' => [],
     'components' => [
         'request' => [
+            'baseUrl'=>'/admin',
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+            'name' => 'advanced',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -37,14 +38,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
+            'scriptUrl'=>'/backend/index.php',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
