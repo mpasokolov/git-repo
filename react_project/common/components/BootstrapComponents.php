e86fb3c0317ce742b99d6d1aace3f18220e7a1b3
mpasokolov <mpasokolov@gmail.com>
2019-03-17 11:00:09 +0300

    some fix

--- react_project/common/components/BootstrapComponents.php
+++ react_project/common/components/BootstrapComponents.php
@@ -16,26 +16,5 @@ use yii\caching\TagDependency;
 
 class BootstrapComponents extends Component {
     public function init() {
-        Event::on(Tasks::class, Tasks::EVENT_AFTER_DELETE, function (Event $event) {
-            $this -> usersTaskSearch($event -> sender);
-        });
-        Event::on(Tasks::class, Tasks::EVENT_AFTER_INSERT, function (Event $event) {
-            $this -> usersTaskSearch($event -> sender);
-        });
-        Event::on(Tasks::class, Tasks::EVENT_AFTER_UPDATE, function (Event $event) {
-            $this -> usersTaskSearch($event -> sender);
-        });
-    }
-
-    private function usersTaskSearch($model) {
-        if (!empty($model -> oldAttributes)) {
-            TagDependency::invalidate(\Yii::$app -> cache, 'user_tasks_search_' . $model -> oldAttributes['id_user']);
-
-            if ($model -> oldAttributes['id_user'] !== $model -> id_user) {
-                TagDependency::invalidate(\Yii::$app -> cache, 'user_tasks_search_' . $model -> id_user);
-            }
-        } else {
-            TagDependency::invalidate(\Yii::$app -> cache, 'user_tasks_search_' . $model -> sender -> id_user);
-        }
     }
 }
